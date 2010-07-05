d092c6caef750c6743fc8bc6ae4dfc97a0d1ea97
Aleksei Kaminin <zendzirou@gmail.com>
2010-07-05 15:46:08 +0400

    Fixed warning in first page loading, without session

--- hook/core/session/flash.php
+++ hook/core/session/flash.php
@@ -1,11 +1,10 @@
 <?php
 $flash = bu::session('flash');
-foreach($flash as $k=>$v){
-    
-    if(!$v['valid'])
-        unset($flash[$k]);
-    else
-        $flash[$k]['valid'] = false;
-}
-
+if(is_array($flash))
+	foreach($flash as $k=>$v){
+		if(!$v['valid'])
+			unset($flash[$k]);
+		else
+			$flash[$k]['valid'] = false;
+	}
 bu::session('flash',$flash);
