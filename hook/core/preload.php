ca08d12b610c87e9d2b516ce93078e3e43040128
Aleksei Kaminin <zendzirou@gmail.com>
2010-07-05 15:53:08 +0400

    More actions in preload hook

--- hook/core/preload.php
+++ hook/core/preload.php
@@ -4,6 +4,8 @@ bu::hook('session/init');
 if (bu::isValidRequest()){
     bu::hook('session/pages');
     bu::hook('session/flash');
+    bu::hook('session/last_post');
 }
 
 bu::hook('php_activerecord');
+bu::lib('helpers/h');
